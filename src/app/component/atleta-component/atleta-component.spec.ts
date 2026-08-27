725fad67e0a3937d58f43937d307f51f3b3f62fa
Ingrid Souza <ingrid.p.souza6@aluno.senai.br>
2026-08-27 16:45:15 -0300

    criado teste para botao

--- src/app/component/atleta-component/atleta-component.spec.ts
+++ src/app/component/atleta-component/atleta-component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AtletaComponent } from './atleta-component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('AtletaComponent', () => {
   let component: AtletaComponent;
@@ -9,6 +10,14 @@ describe('AtletaComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AtletaComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParams: of({})
+          }
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(AtletaComponent);
@@ -18,5 +27,17 @@ describe('AtletaComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve executar o salvar ao clicar em Cadastrar', () => {
+    const salvarSpy = vi.spyOn(component, 'salvar');
+
+    const botao = fixture.nativeElement.querySelector(
+      'button[type="submit"]'
+    );
+
+    botao.click();
+
+    expect(salvarSpy).toHaveBeenCalled();
   });
 });
